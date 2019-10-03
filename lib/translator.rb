ca96b01769c557879795178fd6f2718432aa6631
soufiane <soufiane.oucherrou@gmail.com>
2019-10-03 22:14:13 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,41 @@
-# require modules here
+require "pry"
+require "yaml"
 
-def load_library
-  # code goes here
+emoticons = YAML.load_file('./lib/emoticons.yml')
+  def load_library(emoticons)
+    emoticons = YAML.load_file('./lib/emoticons.yml')
+    final_hash={
+      get_meaning: {},
+      get_emoticon: {}
+    }
+  emoticons.each do |menaing, eglish_japanaise_emoji|
+final_hash[:get_emoticon][eglish_japanaise_emoji[0]] = eglish_japanaise_emoji[1]
+final_hash[:get_meaning][eglish_japanaise_emoji[1]] = menaing
+  end
+ final_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
+def get_japanese_emoticon(emoticons, target)
+  new_hash=load_library(emoticons)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+  new_hash[:get_emoticon].each do |english, japain|
+ if english == target
+   return japain
+ end
+ end
+ return "Sorry, that emoticon was not found"
 
-def get_english_meaning
-  # code goes here
+end
+  
+
+
+def get_english_meaning(emoticons,target)
+  library=load_library(emoticons)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+    library[:get_meaning].each do |k,v|
+      if k == target
+        return v
+      end
+    end
+    return  "Sorry, that emoticon was not found"
 end
