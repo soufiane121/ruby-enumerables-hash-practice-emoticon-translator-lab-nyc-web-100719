1df6cdf33ca742855a2ecd9fec900c7c77e06c88
soufiane <soufiane.oucherrou@gmail.com>
2019-10-03 20:32:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(emoticons)
   emoticons = YAML.load_file('./lib/emoticons.yml')
   emoticons["get_meaning"] = {}
   emoticons["get_emoticon"] = {}
-  p emoticons
+  p emoticons.keys
 end
 
 def get_japanese_emoticon
